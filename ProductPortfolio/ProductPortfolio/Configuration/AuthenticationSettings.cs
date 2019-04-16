d6e87e454322a8c9c8f67b917f9c7305ca29a541
nlangridge <nick.langridge@gmail.com>
2019-04-16 08:16:28 +0100

    Latest changes

--- ProductPortfolio/ProductPortfolio/Configuration/AuthenticationSettings.cs
+++ ProductPortfolio/ProductPortfolio/Configuration/AuthenticationSettings.cs
@@ -4,6 +4,9 @@ namespace ProductPortfolio.Configuration
 {
     public class AuthenticationSettings
     {
+        /// <summary>
+        /// Valid Api keys
+        /// </summary>
         public List<string> ApiKeys { get; set; }
     }
 }
